f225b96f13a7e19fa05ddd71cae5c2afc0733d87
Victor Picon <revemvi@hotmail.com>
2024-01-13 16:33:03 -0300

    Fix: fixed tip calculator texts

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-print('Bem vindo ao calculador de gorgeta.')
-bill = float(input("Qual foi o total da conta? $"))
+print("Bem vindo ao calculador de gorgeta.")
+bill = float(input("Qual foi o total da conta? R$"))
 tip = int(input("Quanto de gorgeta você deseja dar? 10, 12 ou 15? "))
 people = int(input("Em quantas pessoas você deseja dividir a conta? "))
-tip_as_percent = tip/100
+tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
-print(f"Cada pessoa terá que pagar ${final_amount}")
-
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Cada pessoa terá que pagar R${final_amount}")
